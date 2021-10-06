3fbb1ad0fbf435d8aacaff6279a67632f0d1df38
tjandy98 <tjandy98@gmail.com>
2021-10-06 21:15:36 +0800

    Created initial project structure

---
+++ RRPSS/src/rrpss/Main.java
@@ -0,0 +1,10 @@
+package rrpss;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RRPSS rrpss = new RRPSS();
+
+	}
+
+}
---
+++ RRPSS/src/rrpss/RRPSS.java
@@ -0,0 +1,17 @@
+package rrpss;
+
+import java.util.*;
+
+public class RRPSS {
+
+	RRPSS() {
+
+		System.out.println("Restaurant Reservation and Point of Sale System");
+
+	}
+
+	public void displayOptions() {
+
+	}
+
+}
