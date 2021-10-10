1b4fb7a82bd99b245e90bcac9f22d380a46270a3
tyf98 <49827907+tyf98@users.noreply.github.com>
2021-10-10 14:35:30 +0800

    Added Class file for Alacarte, Dessert and PromotionalPackages

---
+++ RRPSS/src/rrpss/Alacarte.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Alacarte {
+    private int numAlacarteItems;
+
+    // declare array of  objects
+    private Drinks[] drinks = new Drinks[10];
+    private Appetisers[] appetiser = new Appetisers[10];
+    private MainCourse[] main = new MainCourse[10];
+    private Dessert[] dessert = new Dessert[10];
+
+    public Alacarte () {
+        // number of alacarte items
+        numAlacarteItems = 10;
+
+        // create alacarte drink, alacarte appetiser, alacarte main and alacarte dessert object
+        for (int i = 0; i < numAlacarteItems; i++) {
+            drinks[i] = new Drinks(i);
+            appetiser[i] = new Appetisers(i);
+            main[i] = new MainCourse(i);
+            dessert[i] = new Dessert(i);
+        }
+    }
+}
---
+++ RRPSS/src/rrpss/PromotionalPackages.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class PromotionalPackages {
+    private int packageID, quantity;
+    private double promotionPrice;
+    private String description;
+    private MenuItem[] packageItems = new MenuItem[];
+
+    
+    public PromotionalPackages() {
+
+    }
+
+
+    public void createPromotion() {
+
+    }
+
+    public void updatePromotion() {
+
+    }
+
+    public void removePromotion() {
+
+    }
+
+    public void addMenu() {
+
+    }
+}
