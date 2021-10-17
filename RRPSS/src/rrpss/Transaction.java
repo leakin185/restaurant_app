413457c12ea9852477a47ea30ccc9048a054e911
tjandy98 <tjandy98@gmail.com>
2021-10-17 19:59:15 +0800

    Update Transaction.java

--- RRPSS/src/rrpss/Transaction.java
+++ RRPSS/src/rrpss/Transaction.java
@@ -7,11 +7,42 @@ public class Transaction {
 	LocalDateTime date;
 	String type;
 	double amount;
-	Transaction(LocalDateTime date, String type, double amount){
+
+	Transaction(LocalDateTime date, String type, double amount) {
 		this.date = date;
 		this.type = type;
 		this.amount = amount;
-		
+
 	}
-	
+
+	public LocalDateTime getDate() {
+
+		return this.date;
+	}
+
+	public String getType() {
+
+		return this.type;
+	}
+
+	public double getAmount() {
+
+		return this.amount;
+	}
+
+	public void setDate(LocalDateTime date) {
+
+		this.date = date;
+	}
+
+	public void setType(String type) {
+
+		this.type = type;
+	}
+
+	public void setAmount(double amount) {
+
+		this.amount = amount;
+	}
+
 }
