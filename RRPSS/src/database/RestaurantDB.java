615971bfd4a8138f61252f0fd26b066b1f3ab239
tjandy98 <tjandy98@gmail.com>
2021-10-22 23:58:09 +0800

    Update RestaurantDB.java

--- RRPSS/src/database/RestaurantDB.java
+++ RRPSS/src/database/RestaurantDB.java
@@ -3,9 +3,16 @@ import java.util.*;
 
 import rrpss.Reservation;
 import rrpss.Customer;
+import rrpss.Table;
+import rrpss.Transaction;
 
 public class RestaurantDB {
     public static ArrayList<Reservation>	reservations;
     public static ArrayList<Reservation>    completedReservations;
     public static ArrayList<Customer>    customers;
+    public static ArrayList<Transaction> transactions;
+    public static ArrayList<Table> tables;
+
+
+
 }
