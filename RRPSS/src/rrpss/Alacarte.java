305dabc22941d4241b358f30862ea4a692b70b1c
tyf98 <49827907+tyf98@users.noreply.github.com>
2021-10-22 15:28:46 +0800

    updates

--- RRPSS/src/rrpss/Alacarte.java
+++ RRPSS/src/rrpss/Alacarte.java
@@ -1,26 +1,8 @@
-package rrpss;
 
-import java.util.Arrays;
+public class Alacarte extends Food {
 
-public class Alacarte {
-    private int numAlacarteItems;
-
-    // declare array of  objects
-    private Drinks[] drinks = new Drinks[10]; 
-    private Appetisers[] appetiser = new Appetisers[10];
-    private MainCourse[] main = new MainCourse[10];
-    private Dessert[] dessert = new Dessert[10];
-
-    public Alacarte () {
-        // number of alacarte items
-        numAlacarteItems = 10;
-
-        // create alacarte drink, alacarte appetiser, alacarte main and alacarte dessert object
-        for (int i = 0; i < numAlacarteItems; i++) {
-            drinks[i] = new Drinks(i);
-            appetiser[i] = new Appetisers(i);
-            main[i] = new MainCourse(i);
-            dessert[i] = new Dessert(i);
-        }
+    public Alacarte (String menuName, String desc, double price, int itemId, CourseType type, Temp temp) {
+        super(menuName, desc, price, itemId, type, temp);
     }
+
 }
--- RRPSS/src/rrpss/PromotionalPackages.java
+++ RRPSS/src/rrpss/PromotionalPackages.java
@@ -1,52 +1,75 @@
-package rrpss;
+import java.util.*;
 
-import java.util.Arrays;
+import rrpss.Food;
 
-public class PromotionalPackages {
-    private int packageID, quantity;
-    private double promotionPrice;
-    private String description;
-    private MenuItem[] packageItems = new MenuItem[];
-    private boolean removed;
+public class PromotionalPackages extends Food {
+
+    private ArrayList<Food> promotionSet;
+
+    public PromotionalPackages(String menuName, String desc, double price, int itemId, CourseType type, Temp temp) {
+        super(menuName, desc, price, itemId, type, temp);
+        this.promotionSet = new ArrayList<Food>();
+    }
+
+    public ArrayList<Food> getPromotionSet() {
+        return promotionSet;
+    }
+
+    public void addToPromotionSet(Food food) {
+        promotionSet.add(food);
+    }
+
+    public void addToPromotionSet(ArrayList<MenuItem> menu) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        System.out.println("Select food to be added to the Promotion Package: ");
+        for (MenuItem menuItem : menu) {
+            if (menuItem instanceof Food) {
+                System.out.println(i + ": " + menuItem.getMenuName());
+                i++;
+            }
+        }
+        
+        int selection = sc.nextInt();
+        try {
+            if (menu.get(selection) instanceof Food) {
+                Food foodSelection = menu.get(selection);
+                this.promotionSet.add(foodSelection);
+                System.out.println(foodSelection.getMenuName() + "added to Promotional Package.");
+            }
+            else {
+                System.out.println("Selection is invalid.");
+            }
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("Selection does not exist.");
+        }
+    }
+
+    public void removeFromPromotionSet(Food food) {
+        promotionSet.remove(food);
+    }
+
+    public void removeFromPromotionSet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select food to be removed from Promotion Package: ");
+        int i = 1;
+        for (Food foood : promotionSet) {
+            System.out.println(i + ": " + foood.getMenuName());
+            i++;
+        }
+        int selection = sc.nextInt();
+        try {
+            String removeSelection = promotionSet.get(selection).getMenuName();
+            promotionSet.remove(selection);
+            System.out.println(removeSelection + " removed from Promotion Package.");
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("Selection is invalid.");
+        }
+    }
 
     
-    public PromotionalPackages(int packageID, MenuItem[] packageItems, /*int quantity,*/ double promotionPrice, String description) {
-        this.packageID = packageID;
-        this.packageItems = packageItems;
-        //this.quantity = quantity;
-        this.promotionPrice = promotionPrice;
-        this.description = description;
-    }
 
 
-    public void createPromotion(int packageID, MenuItem[] packageItems, /*int quantity,*/ double promotionPrice, String description) {
-        this.packageID = packageID;
-        //this.quantity = quantity;
-        this.packageItems = packageItems;
-        this.promotionPrice = promotionPrice;
-        this.description = description;
-    }
-
-    public void updatePromotion(Object o, MenuItem[] packageItems, double promotionPrice, String description) {
-        if (o.getClass() == this.getClass()) {
-            if (o.packageID == this.packageID) {
-                this.packageItems = packageItems;
-                //this.quantity = quantity;
-                this.promotionPrice = promotionPrice;
-                this.description = description;
-            }
-        }
-    }
-
-    public void removePromotion(Object o) {
-        if (o.getClass() == this.getClass()) {
-            if (o.packageID == this.packageID) {
-                
-            }
-        }
-    }
-
-    public void addMenu() {
-
-    }
 }
