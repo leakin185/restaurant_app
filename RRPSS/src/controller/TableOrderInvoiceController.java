f2223240a0d89dbeededa2e61561d105d1687dc6
Chewposhi <chewposhi@gmail.com>
2021-10-30 18:36:33 +0800

    Update TableOrderInvoiceController.java

--- RRPSS/src/controller/TableOrderInvoiceController.java
+++ RRPSS/src/controller/TableOrderInvoiceController.java
@@ -2,18 +2,23 @@ package controller;
 
 import database.RestaurantDB;
 import controller.TableController;
+import controller.FoodMenuController;
 import rrpss.Table;
 import rrpss.Order;
 import rrpss.orderItem;
+import rrpss.MenuItem;
 
 public class TableOrderInvoiceController {
 	public boolean setOrderToTable(int staffID, int tableID){
 		//TableController TC = new TableController();
 		Table table = TableController.getTableFromTableNo(tableID);
-		
+		//if order for this table is empty
 		if(table.getOrder() == null) {
 			Order order = new Order(staffID,tableID);
-			
+			//print the menu
+			for(MenuItem item : RestaurantDB.menu) {
+				
+			}
 			table.setOrder(null);
 		}
 		
