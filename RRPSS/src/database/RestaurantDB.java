3688fbd2f7135dd12d5f98ad76e7e8aec1046e2f
tjandy98 <tjandy98@gmail.com>
2021-10-30 11:09:10 +0800

    Initialized ArrayLists in RestaurantDB.java

--- RRPSS/src/database/RestaurantDB.java
+++ RRPSS/src/database/RestaurantDB.java
@@ -1,4 +1,5 @@
 package database;
+
 import java.util.*;
 
 import rrpss.Reservation;
@@ -8,13 +9,12 @@ import rrpss.Transaction;
 import rrpss.Staff;
 
 public class RestaurantDB {
-    public static ArrayList<Reservation>	reservations;
-    public static ArrayList<Reservation>    completedReservations;
-    public static ArrayList<Customer>    customers;
-    public static ArrayList<Transaction> transactions;
-    public static ArrayList<Table> tables;
-    public static ArrayList<Staff> staffs;
-
+    public static ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+    public static ArrayList<Reservation> completedReservations = new ArrayList<Reservation>();
+    public static ArrayList<Customer> customers = new ArrayList<Customer>();
+    public static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    public static ArrayList<Table> tables = new ArrayList<Table>();
+    public static ArrayList<Staff> staffs = new ArrayList<Staff>();
 
 
 }
