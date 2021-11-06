c32a59a59b1a9a924d1ffb40c6e18067c38c8507
Chewposhi <chewposhi@gmail.com>
2021-11-06 20:58:31 +0800

    Update TableOrderInvoiceController.java

--- RRPSS/src/controller/TableOrderInvoiceController.java
+++ RRPSS/src/controller/TableOrderInvoiceController.java
@@ -107,91 +107,93 @@ public class TableOrderInvoiceController {
 			c = sc.nextInt();
 			//2 branch: 1 add item, 2 edit item
 			Order order = TableController.getTableFromTableNo(tableID).getOrder();
-			switch(c) {
-			case 1:
-				System.out.println("Chose menu item to add");
-				System.out.println(" ");
-				while (true) {
+			while(c!=0) {
+				switch(c) {
+				case 1:
+					System.out.println("Chose menu item to add");
 					System.out.println(" ");
-					System.out.println("Enter menu item ID: \n0 to go back: ");
-					int id = sc.nextInt();
-					MenuItem selected_item = null;
-					if (id == 0)
-						break;
-					// check if ID valid and retrieve item
-					for (MenuItem item : RestaurantDB.menu) {
-						if (item.getItemId() == id) {
-							selected_item = item;
-							System.out.println(" ");
-							System.out.println("item name: "+selected_item.getMenuName());
-							//check alacarte or pakcage
-							if(item instanceof Food) System.out.println("Type: Alacarte");
-							if(item instanceof PromotionalPackages) System.out.println("Type: Promotional Package");
-							System.out.println(" ");
+					while (true) {
+						System.out.println(" ");
+						System.out.println("Enter menu item ID: \n0 to go back: ");
+						int id = sc.nextInt();
+						MenuItem selected_item = null;
+						if (id == 0)
 							break;
+						// check if ID valid and retrieve item
+						for (MenuItem item : RestaurantDB.menu) {
+							if (item.getItemId() == id) {
+								selected_item = item;
+								System.out.println(" ");
+								System.out.println("item name: "+selected_item.getMenuName());
+								//check alacarte or pakcage
+								if(item instanceof Food) System.out.println("Type: Alacarte");
+								if(item instanceof PromotionalPackages) System.out.println("Type: Promotional Package");
+								System.out.println(" ");
+								break;
+							}
+						}
+						if (selected_item == null) {
+							System.out.println("Invalid ID");
+							System.out.println(" ");
+						}
+						else {
+							// add item into order
+							int quantity = 0;
+							System.out.println("Enter quantity");
+							quantity = sc.nextInt();
+							if(selected_item instanceof Food) 
+								order = order.addOrderItem(order, selected_item, quantity);
+							if(selected_item instanceof PromotionalPackages)
+								order = order.addOrderItem(order, (PromotionalPackages)selected_item, quantity);
+							table.setOrder(order);
+							
 						}
 					}
-					if (selected_item == null) {
-						System.out.println("Invalid ID");
-						System.out.println(" ");
+					break;
+				
+				case 2:
+					int menuID=-1;
+					int found = 0;
+					int index = 0;
+					orderItem selected_item = null;
+					System.out.println("Enter menu item ID that you wish to edit\n\n");
+					menuID = sc.nextInt();
+					for(orderItem item : table.getOrder().getOrderItems()) {
+						if(item.getItem().getItemId() == menuID) {
+							found =1;
+							selected_item = item;
+							System.out.println("Found order item: "+selected_item.getItem().getMenuName()+" qauntity: "+selected_item.getQuantity());
+							break;
+						}
+						index++;
 					}
-					else {
-						// add item into order
-						int quantity = 0;
-						System.out.println("Enter quantity");
-						quantity = sc.nextInt();
-						if(selected_item instanceof Food) 
-							order = order.addOrderItem(order, selected_item, quantity);
-						if(selected_item instanceof PromotionalPackages)
-							order = order.addOrderItem(order, (PromotionalPackages)selected_item, quantity);
-						table.setOrder(order);
+					if(found == 1) {
+						//2 branches, 1: edit quantity 2: delete
+						int newQ = selected_item.getQuantity();
+						System.out.println("Enter new quantity (>0) to change order item quantity\n0: Delete order item\n");
+						newQ = sc.nextInt();
+						if(newQ >=1) {
+							selected_item.setQuantity(newQ);
+							System.out.println("new quantity set to "+newQ+"\n");
+						}
+						else {
+							table.getOrder().getOrderItems().remove(index);
+							System.out.println("Order item removed!\n\n");
+						}
 						
 					}
-				}
-				break;
-			
-			case 2:
-				int menuID=-1;
-				int found = 0;
-				int index = 0;
-				orderItem selected_item = null;
-				System.out.println("Enter menu item ID that you wish to edit\n\n");
-				menuID = sc.nextInt();
-				for(orderItem item : table.getOrder().getOrderItems()) {
-					if(item.getItem().getItemId() == menuID) {
-						found =1;
-						selected_item = item;
-						System.out.println("Found order item: "+selected_item.getItem().getMenuName()+" qauntity: "+selected_item.getQuantity());
-						break;
-					}
-					index++;
-				}
-				if(found == 1) {
-					//2 branches, 1: edit quantity 2: delete
-					int newQ = selected_item.getQuantity();
-					System.out.println("Enter new quantity (>0) to change order item quantity\n0: Delete order item\n");
-					newQ = sc.nextInt();
-					if(newQ >=1) {
-						selected_item.setQuantity(newQ);
-						System.out.println("new quantity set to "+newQ+"\n");
-					}
-					else {
-						table.getOrder().getOrderItems().remove(index);
-						System.out.println("Order item removed!\n\n");
-					}
+					else System.out.println("No such menu item in this order.\n\n");
+					break;
 					
+				case 0:
+					return false;
+					
+				default:
+					break;
 				}
-				else System.out.println("No such menu item in this order.\n\n");
-				break;
-				
-			case 0:
-				return false;
-				
-			default:
-				break;
+				System.out.println("1: Add item to order\n2: edit order item\n0: go back\n");
+				c = sc.nextInt();
 			}
-			System.out.println("1: Add item to order\n2: edit order item\n0: go back\n\n");
-			c = sc.nextInt();
 		}
 
 		return false;
