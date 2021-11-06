a0c2d17f21dbe38dbcdf699a8c2edf632893ebae
Chewposhi <chewposhi@gmail.com>
2021-11-06 21:14:03 +0800

    Update orderItem.java

--- RRPSS/src/rrpss/orderItem.java
+++ RRPSS/src/rrpss/orderItem.java
@@ -1,12 +1,14 @@
 package rrpss;
 
 public class orderItem {
-	private static int orderItemID = 1;
+	private static int index = 1;
+	private int orderItemID = 1;
 	private MenuItem item;
 	private int quantity;
 	private boolean isPromotion; 
 	
 	public orderItem(MenuItem item, int quantity, boolean isPromotion) {
+		this.orderItemID = index++;
 		this.orderItemID = orderItemID++;
 		this.item = item;
 		this.quantity = quantity;
