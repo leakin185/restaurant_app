017d01c3d2e35b799e1af3f1ff0a3108478c0678
Chewposhi <chewposhi@gmail.com>
2021-11-09 01:09:23 +0800

    Update Member.java

--- RRPSS/src/rrpss/Member.java
+++ RRPSS/src/rrpss/Member.java
@@ -1,12 +1,38 @@
 package rrpss;
-
+/**
+ * Represent a member of the restaurant
+ * @author Chew Poshi
+ *
+ */
 public class Member {
+	/**
+	 * ID of a membership
+	 */
 	private int ID;
+	/**
+	 * name of a member
+	 */
 	private String name;
+	/**
+	 * phone number of the member
+	 */
 	private int phone;
+	/**
+	 * membership type of the member, silver, gold and platinum
+	 */
 	private String memberType;
+	/**
+	 * discount rate for the member
+	 */
 	private double discount;
-	
+	/**
+	 * Constructor for the member object
+	 * @param ID
+	 * @param name
+	 * @param phone
+	 * @param memberType
+	 * @param discount
+	 */
 	public Member(int ID, String name, int phone, String memberType, double discount) {
 		this.ID = ID;
 		this.name = name;
@@ -15,23 +41,38 @@ public class Member {
 		this.discount = discount;
 		
 	}
-	
+	/**
+	 * return the ID of a member
+	 * @return
+	 */
 	public int getMemberID(){
 		return ID;
 	}
-	
+	/**
+	 * return the name of a member
+	 * @return
+	 */
 	public String getMemberName(){
 		return name;
 	}
-	
+	/**
+	 * return the phone number of a member
+	 * @return
+	 */
 	public int getMemberPhone(){
 		return phone;
 	}
-	
+	/**
+	 * return the membership type of a member
+	 * @return
+	 */
 	public String getMemberType(){
 		return memberType;
 	}
-	
+	/**
+	 * return the discount of a member
+	 * @return
+	 */
 	public double getMemberDiscount(){
 		return discount;
 	}
