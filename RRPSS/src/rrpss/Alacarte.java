e0766778b1ca1fe041565f12a6c14973b9242149
tyf98 <49827907+tyf98@users.noreply.github.com>
2021-11-11 20:07:14 +0800

    Update Alacarte.java

--- RRPSS/src/rrpss/Alacarte.java
+++ RRPSS/src/rrpss/Alacarte.java
@@ -1,6 +1,20 @@
 package rrpss;
+/**
+ * 
+ * Represents a Alacarte Menu Item
+ * @author tyf
+ *
+ */
 public class Alacarte extends Food {
-
+	/**
+	 * Constructor for the object
+	 * @param menuName
+	 * @param desc
+	 * @param price
+	 * @param itemId
+	 * @param type
+	 * @param temp
+	 */
     public Alacarte (String menuName, String desc, double price, int itemId, CourseType type, Temp temp) {
         super(menuName, desc, price, itemId, type, temp);
     }
