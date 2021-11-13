88ea5e508b03c06563a093d8b4e7debe16c515bb
Chewposhi <chewposhi@gmail.com>
2021-11-14 00:06:19 +0800

    Update RestaurantDB.java

--- RRPSS/src/database/RestaurantDB.java
+++ RRPSS/src/database/RestaurantDB.java
@@ -15,12 +15,33 @@ import rrpss.Member;
  *
  */
 public class RestaurantDB {
+	/**
+	 * ArrayList of reservations that are active
+	 */
     public static ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+    /**
+     * ArrayList of completed reservations
+     */
     public static ArrayList<Reservation> completedReservations = new ArrayList<Reservation>();
+    /**
+     * ArrayList of transactions that are made in the restaurant
+     */
     public static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    /**
+     * ArrayList of tables that exist in the restaurant
+     */
     public static ArrayList<Table> tables = new ArrayList<Table>();
+    /**
+     * ArrayList of staffs who work in the restaurant
+     */
     public static ArrayList<Staff> staffs = new ArrayList<Staff>();
+    /**
+     * ArrayList of menu items that are in the restaurant's menu
+     */
     public static ArrayList<MenuItem> menu = new ArrayList<MenuItem>();
+    /**
+     * ArrayList of registered members
+     */
     public static ArrayList<Member> members = new ArrayList<Member>();
 
 
